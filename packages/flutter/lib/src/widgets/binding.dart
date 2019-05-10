278135becfc6edec7e764b154af1042de2a8d60c
Krzysztof Sroka <kpsroka@users.noreply.github.com>
2019-05-10 18:11:43 +0200

    Revert "Calling flushLayout on attachRootWidget instead of initializing ScrollPosition's extents."
    This reverts commit 48afad49d62b21f711c591823c916c60525ddc34.

--- packages/flutter/lib/src/widgets/binding.dart
+++ packages/flutter/lib/src/widgets/binding.dart
@@ -734,7 +734,6 @@ mixin WidgetsBinding on BindingBase, SchedulerBinding, GestureBinding, RendererB
       debugShortDescription: '[root]',
       child: rootWidget,
     ).attachToRenderTree(buildOwner, renderViewElement);
-    pipelineOwner.flushLayout();
   }
 
   /// Whether the [renderViewElement] has been initialized.
